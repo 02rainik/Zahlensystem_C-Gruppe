b7f8cc19a8e8f98954e9ab9906b3177dd15ea025
Ritschinator <61366360+ritschinator@users.noreply.github.com>
2020-04-24 11:23:26 +0200

    Menue

--- Zahlensysteme_Gruppe_C/Zahlensysteme_Gruppe_C/Program.cs
+++ Zahlensysteme_Gruppe_C/Zahlensysteme_Gruppe_C/Program.cs
@@ -13,8 +13,29 @@ namespace Zahlensysteme_Gruppe_C
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Auswahl zwischen Hex ind Bin
+        /// </summary>
+        /// <param name="Eingabe">Benuzer Eingabe</param>
+        static void Switch(string Eingabe = " ")
+        {
+            switch (Eingabe)
+            {
+                case "Hex":
+                    //Methode Hex
+                    break;
+
+                case "Bin":
+                    //Methode Bin
+                    break;
+
+                default:
+                    Console.WriteLine("Error 2");
+                    break;
+            }
         }
     }
 }
