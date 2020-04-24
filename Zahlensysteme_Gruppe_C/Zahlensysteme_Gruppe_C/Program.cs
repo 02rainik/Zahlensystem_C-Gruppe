7ee659f9f71f71e90b95cf939cc2e5c79b4afa21
02rainik <02rainik@rgtfo-me.it>
2020-04-24 10:57:20 +0200

    ReadKey

---
+++ Zahlensysteme_Gruppe_C/Zahlensysteme_Gruppe_C/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zahlensysteme_Gruppe_C
+{
+    class Program
+    {
+        /// <summary>
+        /// Zahlensysteme_Gruppe_C
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+    }
+}
