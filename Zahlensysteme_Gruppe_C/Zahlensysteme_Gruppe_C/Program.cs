f4cde4e619e92db54c24db80382ecd7217c198b3
02rainik <02rainik@rgtfo-me.it>
2020-04-25 11:50:01 +0200

    Alles neu, Alles gut

--- Zahlensysteme_Gruppe_C/Zahlensysteme_Gruppe_C/Program.cs
+++ Zahlensysteme_Gruppe_C/Zahlensysteme_Gruppe_C/Program.cs
@@ -7,35 +7,23 @@ namespace Zahlensysteme_Gruppe_C
 {
     class Program
     {
+        static string Eingabezahl,Eingabemodus;
         /// <summary>
         /// Zahlensysteme_Gruppe_C
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Console.WriteLine("Titel");
+            Console.WriteLine("Bitte Geben Sie die Binärzahl ein");         
+            Eingabezahl = Console.ReadLine();
+            Console.WriteLine("In welches Zahlensystem wollen sie ihre Zahl konvertieren?(Hex/Dec)");
+            Eingabemodus = Console.ReadLine();
+
             Console.ReadKey();
         }
 
-        /// <summary>
-        /// Auswahl zwischen Hex ind Bin
-        /// </summary>
-        /// <param name="Eingabe">Benuzer Eingabe</param>
-        static void Switch(string Eingabe = " ")
-        {
-            switch (Eingabe)
-            {
-                case "Hex":
-                    //Methode Hex
-                    break;
-
-                case "Bin":
-                    //Methode Bin
-                    break;
-
-                default:
-                    Console.WriteLine("Error 2");
-                    break;
-            }
+       
         }
     }
 }
